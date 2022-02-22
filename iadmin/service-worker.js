71b4795a80e356714d36ffe4a0b7d56a4c7e8c04
msolano <msolano@imaginacolombia.com>
2022-02-22 12:37:12 -0500

    +Update: AdminLte | Iadmin Release 2.4.53

--- iadmin/service-worker.js
+++ iadmin/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.661151ca0012150426dd005000367769.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.bcfbe0ba508a82ccc022211dbf429c97.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
