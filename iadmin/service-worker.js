db549202428f1fff50e641dc49cda2d877d001f2
msolano <msolano@imaginacolombia.com>
2022-05-14 15:30:07 -0500

    Update: CMS Version 2.5.13

--- iadmin/service-worker.js
+++ iadmin/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.537d80c514061c6983f0edc638597358.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.63604812e2a517d44ad2460264333820.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
