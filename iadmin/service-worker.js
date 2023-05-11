fe1a40dabbaced3b8e7620881081a4130dd9c356
imagina <msolano2505@gmail.com>
2023-05-11 17:53:42 -0500

    +Release: Iadmin

--- iadmin/service-worker.js
+++ iadmin/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.5a9fc484302c53dac0bd1c132844c462.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.94439df80d7c00bee80c833bd5bbb48b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
