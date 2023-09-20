cc19a46cb49f83123cac44b497f41f4234b795f8
imagina <carlosdevia@imaginacolombia.com>
2023-09-20 15:40:51 +0000

    +Release: Iadmin

--- iadmin/service-worker.js
+++ iadmin/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.a91ea56708cf2f303314416dcdf8f14f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.ed921ddb967e3d8221bd14db3b1158c6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
